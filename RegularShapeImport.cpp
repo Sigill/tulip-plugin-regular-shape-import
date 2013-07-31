c19869dd84126aecf35d1423de898cbb3770cc15
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2013-07-31 10:51:32 +0200

    Add number of nodes, radius and frequency parameters.

--- RegularShapeImport.cpp
+++ RegularShapeImport.cpp
@@ -16,12 +16,45 @@ namespace {
 const char *ShapeList = SHAPELIST;
 
 const char *paramHelp[] = {
-	HTML_HELP_OPEN()
-		HTML_HELP_DEF("Type", "String")
-		HTML_HELP_DEF("Values", SHAPELIST)
-		HTML_HELP_DEF("Default", "Circle")
-		HTML_HELP_BODY()
-		"The shape you want to build."
+	// 0
+	HTML_HELP_OPEN() \
+		HTML_HELP_DEF("Type", "String") \
+		HTML_HELP_DEF("Values", SHAPELIST) \
+		HTML_HELP_DEF("Default", "Circle") \
+		HTML_HELP_BODY() \
+		"The shape you want to build." \
+		HTML_HELP_CLOSE(),
+
+	// 1
+	HTML_HELP_OPEN() \
+		HTML_HELP_DEF("Type", "Unsigned int") \
+		HTML_HELP_DEF("Default", "100") \
+		HTML_HELP_BODY() \
+		"Number of nodes." \
+		HTML_HELP_CLOSE(),
+
+	// 2
+	HTML_HELP_OPEN() \
+		HTML_HELP_DEF("Type", "Float") \
+		HTML_HELP_DEF("Default", "1") \
+		HTML_HELP_BODY() \
+		"Radius." \
+		HTML_HELP_CLOSE(),
+
+	// 3
+	HTML_HELP_OPEN() \
+		HTML_HELP_DEF("Type", "Unsigned int") \
+		HTML_HELP_DEF("Default", "10") \
+		HTML_HELP_BODY() \
+		"Frequency." \
+		HTML_HELP_CLOSE(),
+
+	// 4
+	HTML_HELP_OPEN() \
+		HTML_HELP_DEF("Type", "Float") \
+		HTML_HELP_DEF("Default", "1.5") \
+		HTML_HELP_BODY() \
+		"Secondary radius." \
 		HTML_HELP_CLOSE()
 	};
 }
@@ -32,12 +65,18 @@ public:
 
 	ShapeGenerator(PluginContext *context) :
 		ImportModule(context) {
-			addInParameter< StringCollection >("Shape", paramHelp[0], ShapeList);
+			addInParameter< StringCollection > ("Shape",            paramHelp[0], ShapeList);
+			addInParameter< unsigned int >     ("number of nodes",  paramHelp[1], "100");
+			addInParameter< float >            ("radius",           paramHelp[2], "1");
+			addInParameter< unsigned int >     ("frequency",        paramHelp[3], "10");
+			addInParameter< float >            ("secondary radius", paramHelp[4], "0.5");
 		}
 	~ShapeGenerator() {}
 
 	bool importGraph() {
 		StringCollection shape;
+		unsigned int number_of_nodes, frequency;
+		float radius, secondary_radius;
 		int i = 0;
 
 		if(dataSet == NULL)
@@ -46,12 +85,34 @@ public:
 		if(!dataSet->get("Shape", shape))
 			throw std::runtime_error(std::string("No \"Shape\" property provided."));
 
-		const int count = 100;
+		if(!dataSet->get("number of nodes", number_of_nodes))
+			throw std::runtime_error(std::string("No \"number of node\" property provided."));
 
-		vector<node> nodes; nodes.reserve(count);
-		graph->addNodes(count, nodes);
-		//vector<pair<node, node> > ends; ends.reserve(count);
-		//vector<edge> edges; edges.reserve(count);
+		if(!dataSet->get("radius", radius))
+			throw std::runtime_error(std::string("No \"radius\" property provided."));
+
+		if(radius <= 0)
+			throw std::runtime_error(std::string("Radius must be > 0."));
+
+		if(shape.getCurrentString().compare("Circle") != 0) {
+			if(!dataSet->get("frequency", frequency))
+				throw std::runtime_error(std::string("No \"frequency\" property provided."));
+
+			if(frequency <= 0)
+				throw std::runtime_error(std::string("Frequency must be > 0."));
+
+
+			if(!dataSet->get("secondary radius", secondary_radius))
+				throw std::runtime_error(std::string("No \"secndary radius\" property provided."));
+
+			if(secondary_radius <= 0)
+				throw std::runtime_error(std::string("Secondary radius must be > 0."));
+		}
+
+		vector<node> nodes; nodes.reserve(number_of_nodes);
+		graph->addNodes(number_of_nodes, nodes);
+		//vector<pair<node, node> > ends; ends.reserve(number_of_nodes);
+		//vector<edge> edges; edges.reserve(number_of_nodes);
 		double x, y, z, theta;
 		LayoutProperty *layout = graph->getProperty<LayoutProperty> ("viewLayout");
 		vector<node>::const_iterator itNodes;
@@ -59,19 +120,19 @@ public:
 		if(shape.getCurrentString().compare("Circle") == 0) {
 			z = 0.0;
 			for(itNodes = nodes.begin(); itNodes < nodes.end(); ++itNodes) {
-				theta = i * 2 * M_PI / (double)count;
-				x = 25 * cos(theta);
-				y = 25 * sin(theta);
+				theta = i * 2 * M_PI / (double)number_of_nodes;
+				x = radius * cos(theta);
+				y = radius * sin(theta);
 				layout->setNodeValue(*itNodes, Coord(x, y, z));
 
 				++i;
 			}
 		} else if(shape.getCurrentString().compare("SinCircle") == 0) {
 			for(itNodes = nodes.begin(); itNodes < nodes.end(); ++itNodes) {
-				theta = i * 2 * M_PI / (double)count;
-				x = 25 * cos(theta);
-				y = 25 * sin(theta);
-				z = 10 * sin(5 * theta);
+				theta = i * 2 * M_PI / (double)number_of_nodes;
+				x = radius * cos(theta);
+				y = radius * sin(theta);
+				z = secondary_radius * sin(frequency * theta);
 				layout->setNodeValue(*itNodes, Coord(x, y, z));
 
 				++i;
@@ -80,13 +141,15 @@ public:
 			double x1, y1;
 
 			for(itNodes = nodes.begin(); itNodes < nodes.end(); ++itNodes) {
-				theta = i * 2 * M_PI / (double)count;
-				x1 = 25 + 5 * cos(10 * theta);
+				theta = i * 2 * M_PI / (double)number_of_nodes;
+				x1 = radius + secondary_radius * cos(frequency * theta);
 				y1 = 0;
-				z = 5 * sin(10 * theta);
-				
+				z = secondary_radius * sin(frequency * theta);
+
+				// Wrapping the spiral as a circle
 				x = x1 * cos(theta) + y1 * sin(theta);
 				y = -x1 * sin(theta) + y1 * cos(theta);
+
 				layout->setNodeValue(*itNodes, Coord(x, y, z));
 
 				++i;
